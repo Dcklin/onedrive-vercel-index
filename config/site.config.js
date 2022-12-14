caf7386b5c151ddd65969bb32ee1f5e689a3b4c4
Dcklin <56791902+Dcklin@users.noreply.github.com>
2022-12-14 17:28:06 +0800

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -38,7 +38,7 @@ module.exports = {
   // [OPTIONAL] The footer component of your website. You can write HTML here, but you need to escape double
   // quotes - changing " to \". You can write anything here, and if you like badges, generate some with https://shields.io
   footer:
-    'Powered by <a href="https://legendsov.serveblog.net/api/raw/?path=/QRcode/qq.jpg" target="_blank" rel="noopener noreferrer">Legendsov</a>.',
+    'Powered by <a href="https://legendsov.serveblog.net/zh-CN/QRcode/qq.jpg" target="_blank" rel="noopener noreferrer">Legendsov</a>.',
 
   // [OPTIONAL] This is where you specify the folders that are password protected. It is an array of paths pointing to all
   // the directories in which you have .password set. Check the documentation for details.
@@ -53,7 +53,7 @@ module.exports = {
   links: [
     {
       name: 'QQ',
-      link: 'https://legendsov.serveblog.net/api/raw/?path=/QRcode/qq.jpg',
+      link: 'https://legendsov.serveblog.net/zh-CN/QRcode/qq.jpg',
     },
   ],
 
